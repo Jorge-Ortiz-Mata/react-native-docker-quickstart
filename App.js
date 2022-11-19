c175b65854d103e00ef118d9312fefac9452810c
Jorge-Ortiz-Mata <ortiz.mata.jorge@gmail.com>
2022-11-19 19:08:28 +0000

    Welcome message.

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontWeight: 600}}>¡Start from here!</Text>
       <StatusBar style="auto" />
     </View>
   );
